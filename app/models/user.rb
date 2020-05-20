852130b36c3abc452212b1c3660b1703dc23bff3
codecell <ezakaphil@gmail.com>
2020-05-20 17:15:25 +0100

    Remove starter comments from Readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5, maximum: 20 }
+
+  has_many :posts
+  has_many :comments
 end
