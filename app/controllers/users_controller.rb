904cc7f13e0bdee69aa2620daefaece6c3e882c3
codecell <ezakaphil@gmail.com>
2020-05-21 17:23:56 +0100

    Add ability to create records using the new form_for format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   include UsersHelper
   
   def new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-    puts @user
+    @user.save
+
     if @user.save
       redirect_to new_user_path
     else
